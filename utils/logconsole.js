9cb57c35a6cb8d4699be4cb49991c347da420497
Leomotors (WSL) <59821765+Leomotors@users.noreply.github.com>
2021-04-07 20:56:11 +0700

    logging to file

--- utils/logconsole.js
+++ utils/logconsole.js
@@ -1,9 +1,23 @@
 // * Function for logging console neatly
 
 let getFormattedTime = require("./time.js")
+const fs = require("fs")
+
+let initstr = `Created at ${getFormattedTime()}\n\n`
+
+fs.writeFile("./temp/log.txt", initstr, (err) => {
+    if (err)
+        console.log(`[LOG ERROR] Error on initing file: ${err}`)
+    console.log("[FILE SUCCESS] Log File Inited")
+})
 
 function logconsole(logmsg, status = "Normal") {
-    console.log(`[${getFormattedTime()}][${status}] ${logmsg}`)
+    let logstr = `[${getFormattedTime()}][${status}] ${logmsg}`
+    console.log(logstr)
+    fs.appendFile("./temp/log.txt", logstr+"\n", (err) => {
+        if (err)
+            console.log(`[LOG ERROR] Error on writing log file: ${err}`)
+    })
 }
 
 module.exports = logconsole
