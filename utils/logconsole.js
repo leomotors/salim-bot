4671c2cf6a2596dc783635d8477b48bf2aa844e9
Leomotors (WSL) <59821765+Leomotors@users.noreply.github.com>
2021-04-07 14:22:28 +0700

    required -> require

--- utils/logconsole.js
+++ utils/logconsole.js
@@ -1,6 +1,6 @@
 // * Function for logging console neatly
 
-let getFormattedTime = required("./time.js")
+let getFormattedTime = require("./time.js")
 
 function logconsole(logmsg, status = "Normal") {
     console.log(`[${getFormattedTime()}][${status}] ${logmsg}`)
