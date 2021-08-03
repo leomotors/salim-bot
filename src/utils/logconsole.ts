3a4dfdc9eaa4dda5a1cd3cc1affd1e7cf378f99f
Leomotors <59821765+Leomotors@users.noreply.github.com>
2021-08-03 15:24:59 +0700

    dir change

--- src/utils/logconsole.ts
+++ src/utils/logconsole.ts
@@ -1,17 +1,17 @@
 // * Function for logging console neatly
 
 import { getFormattedTime } from "./time"
-import { writeFile, appendFile } from "fs"
-import { yellow, red } from "chalk"
+import fs from "fs"
+import chalk from "chalk"
 import { do_log } from "../../bot_settings.json"
 
 let initstr = `Created at ${getFormattedTime()}\n\n`
 let initfname = `${getFormattedTime(true)}.txt`
 
 if (do_log) {
-    writeFile(`./log/${initfname}`, initstr, (err: Error) => {
+    fs.writeFile(`./log/${initfname}`, initstr, (err: Error) => {
         if (err)
-            console.log(red(`[LOG ERROR] Error on initing file: ${err}`))
+            console.log(chalk.red(`[LOG ERROR] Error on initing file: ${err}`))
         console.log("[FILE SUCCESS] Log File Inited")
     })
 }
@@ -21,15 +21,15 @@ export function logconsole(logmsg: string, status = "Normal") {
 
     // * Color Source: https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
     if (status.includes("ERROR") || status.includes("EXCEPTION"))
-        console.log(red(logstr))
+        console.log(chalk.red(logstr))
     else if (status.includes("WARNING"))
-        console.log(yellow(logstr))
+        console.log(chalk.yellow(logstr))
     else
         console.log(logstr)
 
     if (do_log)
-        appendFile(`./log/${initfname}`, logstr + "\n", (err: Error) => {
+        fs.appendFile(`./log/${initfname}`, logstr + "\n", (err: Error) => {
             if (err)
-                console.log(red(`[LOG ERROR] Error on writing log file: ${err}`))
+                console.log(chalk.red(`[LOG ERROR] Error on writing log file: ${err}`))
         })
 }
