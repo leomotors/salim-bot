3912abe9b1faaedb073cce330479a9337cb67fd0
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-10-07 21:02:36 +0700

    Cleaned Old Version

--- src/bot.ts
+++ src/bot.ts
@@ -1,28 +1,6 @@
-/**
- * * bot.ts -> Main of Bot (Run this with npm start)
- */
-
-import BotClient from "./client/Client";
-import Console from "./console/Console";
-import Import from "./import/Import";
-import Logger from "./utils/Logger";
-import Response from "./responses/Response";
-
 import dotenv from "dotenv";
 dotenv.config();
 
-// * Initialize Begin
-Import();
+import { SBotClient } from "s-bot-framework";
 
-Logger.construct();
-
-const client: BotClient = new BotClient();
-const msgHandler = new Response();
-
-Console.construct(client);
-// * Initialize End
-
-client.attemptLogin();
-client.implementsResponse(msgHandler);
-
-Logger.log("[SYNC SETUP COMPLETE] bot.ts control reach the end of file", "SUCCESS", false);
+const client = new SBotClient();
